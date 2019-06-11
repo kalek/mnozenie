83c57ad5c122487a6d35936b6f9e04cb8efe2be6
Damian Kalek <damian@ucreate.it>
2019-06-11 14:27:18 +0200

    Update

---
+++ build/precache-manifest.885830110a43e17f3e49c8e8495032d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f245cfab4ba38a75756e",
+    "url": "/static/js/main.5ad52493.chunk.js"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "f245cfab4ba38a75756e",
+    "url": "/static/css/main.1d52a6fe.chunk.css"
+  },
+  {
+    "revision": "d07f93e8b1289b4cf7451013d4601715",
+    "url": "/index.html"
+  }
+];
